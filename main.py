e1531e1874750235ac3e4869cb7db4cea73f8085
Emmanuel Adeniyi <emmanueladeniyi@Emmanuel-MacBook-Pro.local>
2022-04-09 18:40:45 +0300

    message

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def outer_function():
+    global a
+    a = 20
+
+    def inner_function():
+        global a
+        a = 30
+        print('a =', a)
+
+    inner_function()
+    print('a =', a)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+a = 10
+outer_function()
+print('a =', a)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
